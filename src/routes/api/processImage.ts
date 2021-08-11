3ba691b3dad4ec4bb698310f13da304e0e1c5dfb
Ajay <ajaychoudhary19990@gmail.com>
2021-08-11 08:31:47 +0530

    Added caching

--- src/routes/api/processImage.ts
+++ src/routes/api/processImage.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import sharp from "sharp";
 import { promises as fsPromises } from "fs";
+import NodeCache from "node-cache";
 
+const nodeCache = new NodeCache();
 const routes = express.Router();
 
 const processImage = routes.use("/", async (req, res) => {
@@ -10,17 +12,25 @@ const processImage = routes.use("/", async (req, res) => {
   const resultFilePath =
     process.cwd() + `/assets/thumb/${req.query.imageName}Thumb.jpg`;
 
+  const width = parseInt(`${req.query.width}`);
+  const height = parseInt(`${req.query.height}`);
+  const imageName = req.query.imageName;
+  const cacheKey = `${imageName}#${width}#${height}`;
+
   try {
     await fsPromises.readFile(sourceFilePath);
   } catch (e) {
     return res.send("Unable to find an image with this image name");
   }
 
-  try {
-    const width = parseInt(`${req.query.width}`);
-    const height = parseInt(`${req.query.height}`);
-    await sharp(sourceFilePath).resize(width, height).toFile(resultFilePath);
+  const cachedResultFilePath = nodeCache.get(cacheKey);
+  if (cachedResultFilePath != undefined) {
+    return res.sendFile(cachedResultFilePath as string);
+  }
 
+  try {
+    await sharp(sourceFilePath).resize(width, height).toFile(resultFilePath);
+    nodeCache.set(cacheKey, resultFilePath);
     return res.sendFile(resultFilePath);
   } catch (e) {
     console.log(e);
